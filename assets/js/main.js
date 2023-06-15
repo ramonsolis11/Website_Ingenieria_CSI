db60e73f82479c55a609e5ec5586949df52f1932
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-06-15 17:34:05 -0600

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,18 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container",{
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
 
 /*=============== VALUE ACCORDION ===============*/
